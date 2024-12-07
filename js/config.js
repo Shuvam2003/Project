425fcd68fa15f3702c08b4cafe63dbdd01bc4598
Shuvam Mondal <108916549+Shuvam2003@users.noreply.github.com>
2024-12-07 09:32:05 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_TTrnaFA2g', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5sdfb626jvd835d1bcvcga5omh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
