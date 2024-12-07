e151876306138123a0e008f7daf98eedde5600af
Shuvam Mondal <108916549+Shuvam2003@users.noreply.github.com>
2024-12-07 09:49:15 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_TTrnaFA2g', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5sdfb626jvd835d1bcvcga5omh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+         clientSecret = '<client secret>'
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
